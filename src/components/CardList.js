2fe5acd9f0ce53d4e1cbafeb7e72da885be182be
alexanderws <Alex.ws@icloud.com>
2020-12-02 17:15:45 +0100

    removed intentional error

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,9 +3,6 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ listToRender }) => {
-  if (true) {
-    throw new Error("HALLA");
-  }
   return listToRender.map((obj, index) => {
     return (
       <Card key={index} id={index} name={obj.name} email={obj.email} />
